e1dfcb275c967bf63676933c640a25bbe236361c
boofm <1kajwf@gmail.com>
2026-01-29 19:08:33 -0500

    update for f3
    updated for f3

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter f2,$(TARGET_DEVICE)),)
+ifneq ($(filter f3,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
--- AndroidProducts.mk
+++
@@ -1,2 +0,0 @@
-PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/omni_f2.mk
